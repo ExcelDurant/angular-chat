ef19058bd1c9c0863208e8524f567f28e5f249dc
ExcelDurant <gnopaexcel@yahoo.fr>
2021-04-15 13:18:14 +0100

    fixing redirection issues on login and signup

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -15,19 +15,19 @@ export class AuthService {
   constructor(
     public afs: AngularFirestore,   // Inject Firestore service
     public afAuth: AngularFireAuth, // Inject Firebase auth service
-    public router: Router,  
+    public router: Router,
     public ngZone: NgZone // NgZone service to remove outside scope warning
-  ) {    
+  ) {
     /* Saving user data in localstorage when 
     logged in and setting up null when logged out */
     this.afAuth.authState.subscribe(user => {
       if (user) {
         this.userData = user;
         localStorage.setItem('user', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('user') || '');
+        JSON.parse(localStorage.getItem('user') || '{}');
       } else {
-        localStorage.setItem('user', '');
-        JSON.parse(localStorage.getItem('user') || '');
+        localStorage.setItem('user', '{}');
+        JSON.parse(localStorage.getItem('user') || '{}');
       }
     })
   }
@@ -70,17 +70,17 @@ export class AuthService {
   // Reset Forggot password
   ForgotPassword(passwordResetEmail: string) {
     return this.afAuth.auth.sendPasswordResetEmail(passwordResetEmail)
-    .then(() => {
-      window.alert('Password reset email sent, check your inbox.');
-    }).catch((error) => {
-      window.alert(error)
-    })
+      .then(() => {
+        window.alert('Password reset email sent, check your inbox.');
+      }).catch((error) => {
+        window.alert(error)
+      })
   }
 
   // Returns true when user is looged in and email is verified
   get isLoggedIn(): boolean {
-    const user = JSON.parse(localStorage.getItem('user') || '');
-    return (user !== '' && user.emailVerified !== false) ? true : false;
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    return (user !== '{}' && user.emailVerified !== false) ? true : false;
   }
 
   // Sign in with Google
@@ -91,21 +91,20 @@ export class AuthService {
   // Auth logic to run auth providers
   AuthLogin(provider: auth.AuthProvider) {
     return this.afAuth.auth.signInWithPopup(provider)
-    .then((result) => {
-       this.ngZone.run(() => {
+      .then((result) => {
+        this.SetUserData(result.user);
+        this.ngZone.run(() => {
           this.router.navigate(['/dashboard']);
-          console.log(this.router.navigate);
         })
-      this.SetUserData(result.user);
-    }).catch((error) => {
-      window.alert(error)
-    })
+      }).catch((error) => {
+        window.alert(error)
+      })
   }
 
   /* Setting up user data when sign in with username/password, 
   sign up with username/password and sign in with social auth  
   provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
-  SetUserData(user:any) {
+  SetUserData(user: any) {
     const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
     const userData: User = {
       uid: user.uid,
